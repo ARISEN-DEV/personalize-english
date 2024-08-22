bf50540dc0604021635c01564c416e0e7ff34248
ARISEN-DEV <rfalconeb@gmail.com>
2024-08-22 19:30:05 -0300

    Updates

--- assets/hoisted.CGyhj8EM.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.BbDQZSCT.js";function t(){const i=window.innerWidth,e=document.getElementById("form");e&&(i<768?e.style.width="100%":e.style.width="75%")}t();window.addEventListener("resize",t);
